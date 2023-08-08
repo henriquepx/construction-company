7f9aff5bbad460152e70ff792310b114eaefb54c
Henrique <henriquepinheiroxavier@gmail.com>
2023-08-08 10:15:51 -0300

    chore: initital repository

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./sections/Header"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/sections/Header.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+
+`
+const Header = () => {
+  return (
+    <HeaderContainer>
+        
+    </HeaderContainer>
+  )
+}
+
+export default Header
